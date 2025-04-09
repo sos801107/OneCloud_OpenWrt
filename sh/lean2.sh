f66ae97610e2da98fce6a71d44c728c4e44e0c33
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-04-09 09:49:55 +0800

    Update lean2.sh

--- sh/lean2.sh
+++ sh/lean2.sh
@@ -39,6 +39,10 @@ sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.defa
 # 修改 argon 为默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
+# istoreos-theme
+rm -rf feeds/luci/themes/luci-theme-argon
+cp -r feeds/theme/luci-theme-argon feeds/luci/themes/luci-theme-argon
+
 # hijack dns queries to router(firewall)
 sed -i '/REDIRECT --to-ports 53/d' package/network/config/firewall/files/firewall.user
 # 把局域网内所有客户端对外ipv4的53端口查询请求，都劫持指向路由器(iptables -n -t nat -L PREROUTING -v --line-number)(iptables -t nat -D PREROUTING 2)
