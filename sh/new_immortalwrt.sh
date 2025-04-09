ab7edc01f3c7e738d4d66eb012cd60fefedb9935
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-04-09 11:27:38 +0800

    Update new_immortalwrt.sh

--- sh/new_immortalwrt.sh
+++ sh/new_immortalwrt.sh
@@ -7,4 +7,7 @@ rm -rf package/libs/uclient
 sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/immortalwrt.tar.gz
 tar zxvfp rmoyulong.tar.gz
 
+echo 'src-git turboacc https://github.com/chenmozhijin/turboacc.git;luci' >> feeds.conf.default
+echo 'src-git turboaccpackage https://github.com/chenmozhijin/turboacc.git;package' >> feeds.conf.default
+echo 'src-git kiddin9 https://github.com/kiddin9/kwrt-packages' >> feeds.conf.default
 echo 'src-git theme https://github.com/sbwml/luci-theme-argon' >> feeds.conf.default
