8ae4adac5091b96ee12672ee20bc81460b891233
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-05-15 16:52:22 +0800

    Update new_immortalwrt.sh

--- sh/new_immortalwrt.sh
+++ sh/new_immortalwrt.sh
@@ -3,7 +3,7 @@
 rm -rf package/libs/mbedtls
 rm -rf package/libs/ustream-ssl
 rm -rf package/libs/uclient
-
+rm -rf package/shadowsocks-libev 
 sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/immortalwrt.tar.gz
 tar zxvfp rmoyulong.tar.gz
 
