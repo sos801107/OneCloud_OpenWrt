fb17869aec84c67bece67fabbae3df5b352d46a0
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-07-27 18:52:53 +0800

    Update remove.sh

--- sh/remove.sh
+++ sh/remove.sh
@@ -1,1 +1,22 @@
 
+rm -rf feeds/packages/net/homeproxy
+rm -rf feeds/luci/applications/luci-app-homeproxy
+rm -rf feeds/kenzo/homeproxy
+rm -rf feeds/kenzo/luci-app-homeproxy
+rm -rf feeds/luci/applications/luci-app-turboacc
+rm -rf feeds/packages/lang/ruby
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/aria2
+rm -rf feeds/packages/net/ariang 
+rm -rf feeds/luci/luci-app-aria2
+###############################
+rm -rf feeds/packages/lang/rust
+rm -rf  package/openwrt-passwall/shadowsocks-rust
+rm -rf  package/openwrt-passwall/hysteria
+####################################################
+#解决shadowsocks-libev编译错误
+rm -rf feeds/packages/net/shadowsocks-libev
+cp -rf ./package/shadowsocks-libev feeds/packages/net/
+#####################################################
