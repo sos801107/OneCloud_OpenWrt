6b6a5fb83119f4f4240a6d859d51ba3c4a2d2ad7
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-07-27 13:36:38 +0800

    Update new_immortalwrt.sh

--- sh/new_immortalwrt.sh
+++ sh/new_immortalwrt.sh
@@ -8,15 +8,11 @@ sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/r
 tar zxvfp rmoyulong.tar.gz
 
 
-#echo 'src-git turboacc https://github.com/chenmozhijin/turboacc.git;luci' >> feeds.conf.default
-#echo 'src-git turboaccpackage https://github.com/chenmozhijin/turboacc.git;package' >> feeds.conf.default
-#echo 'src-git nftables https://github.com/fullcone-nat-nftables/nft-fullcone' >> feeds.conf.default
-
-echo 'src-git appvlmcsd https://github.com/AutoCONFIG/luci-app-vlmcsd;master' >> feeds.conf.default
-
-#echo 'src-git theme https://github.com/zijieKwok/istoreos-theme' >> feeds.conf.default
+echo 'src-git turboacc https://github.com/chenmozhijin/turboacc.git;luci' >> feeds.conf.default
+echo 'src-git turboaccpackage https://github.com/chenmozhijin/turboacc.git;package' >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 echo 'src-git theme https://github.com/sbwml/luci-theme-argon;openwrt-24.10' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small;master' >> feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages;master' >> feeds.conf.default
-
 
