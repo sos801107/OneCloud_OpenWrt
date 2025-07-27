a44d77efecca736adeb63dd29a271d4a0092906e
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-07-27 16:42:26 +0800

    Update immortalwrt.sh

--- sh/immortalwrt.sh
+++ sh/immortalwrt.sh
@@ -161,13 +161,13 @@ function merge_package() {
 
 git_sparse_clone openwrt-24.10 https://github.com/sbwml/luci-theme-argon luci-theme-argon
 git_sparse_clone openwrt-24.10 https://github.com/sbwml/luci-theme-argon luci-app-argon-config
-git_sparse_clone main https://github.com/xiaorouji/openwrt-passwall luci-app-passwall
-git_sparse_clone luci https://github.com/chenmozhijin/turboacc luci-app-turboacc
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-passwall
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-turboacc
 
 #git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-mosdns
 #git_sparse_clone main https://github.com/kiddin9/kwrt-packages mosdns
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-tailscale
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages tailscale
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-tailscale
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages tailscale
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-upnp
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages miniupnpd
 
@@ -185,10 +185,10 @@ pushd feeds/luci
    curl -s https://raw.githubusercontent.com/oppen321/path/refs/heads/main/Firewall/0001-luci-mod-status-firewall-disable-legacy-firewall-rul.patch | patch -p1
 popd
 
-pushd
-   curl -sSL https://raw.githubusercontent.com/Jaykwok2999/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-popd
+#pushd
+#   curl -sSL https://raw.githubusercontent.com/Jaykwok2999/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+#popd
 
 
-./scripts/feeds update -a
-./scripts/feeds install -a
+#./scripts/feeds update -a
+#./scripts/feeds install -a
