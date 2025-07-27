22e215cdd63aeb6078030b9f763df2f67080a6bf
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-07-27 20:51:50 +0800

    Update new_immortalwrt.sh

--- sh/new_immortalwrt.sh
+++ sh/new_immortalwrt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#rm -rf package/libs/mbedtls
-#rm -rf package/libs/ustream-ssl
-#rm -rf package/libs/uclient
+rm -rf package/libs/mbedtls
+rm -rf package/libs/ustream-ssl
+rm -rf package/libs/uclient
 
-#sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/immortalwrt.tar.gz
-#tar zxvfp rmoyulong.tar.gz
+sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/immortalwrt.tar.gz
+tar zxvfp rmoyulong.tar.gz
 
 
 #echo 'src-git turboacc https://github.com/chenmozhijin/turboacc.git;luci' >> feeds.conf.default
