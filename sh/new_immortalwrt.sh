834ae23db1a9b1f18fa5d1c86aba5c3b63c509d2
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-07-27 16:39:05 +0800

    Update new_immortalwrt.sh

--- sh/new_immortalwrt.sh
+++ sh/new_immortalwrt.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-rm -rf package/libs/mbedtls
-rm -rf package/libs/ustream-ssl
-rm -rf package/libs/uclient
+#rm -rf package/libs/mbedtls
+#rm -rf package/libs/ustream-ssl
+#rm -rf package/libs/uclient
 
-sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/immortalwrt.tar.gz
-tar zxvfp rmoyulong.tar.gz
+#sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/immortalwrt.tar.gz
+#tar zxvfp rmoyulong.tar.gz
 
 
-echo 'src-git turboacc https://github.com/chenmozhijin/turboacc.git;luci' >> feeds.conf.default
-echo 'src-git turboaccpackage https://github.com/chenmozhijin/turboacc.git;package' >> feeds.conf.default
+#echo 'src-git turboacc https://github.com/chenmozhijin/turboacc.git;luci' >> feeds.conf.default
+#echo 'src-git turboaccpackage https://github.com/chenmozhijin/turboacc.git;package' >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 echo 'src-git theme https://github.com/sbwml/luci-theme-argon;openwrt-24.10' >> feeds.conf.default
